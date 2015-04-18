411c486f10826163b4e00e09a69c0713e409ebbb
wxu <winnielandau@gmail.com>
2015-04-17 20:04:57 -0700

    remove JQuery AJAX file

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,7 +1,7 @@
 //Place all the behaviors and hooks related to the matching controller here.
 //All this logic will automatically be available in application.js.
 //You can use CoffeeScript in this file: http://coffeescript.org/
-
+/*
 $(document).ready(function() {
 
   var check_search = function(evt) {
@@ -18,7 +18,7 @@ $(document).ready(function() {
   };
 
 });
-
+*/
 /*
   $.ajax({
     type: "POST",
